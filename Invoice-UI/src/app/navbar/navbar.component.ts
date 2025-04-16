8c3cb22910d718276a3076a45e5f13ef070c426a
[200~JkjupiterKing <jupiterkingtechnologies@gmail.com>
2025-04-16 13:30:50 +0530

    Updated the navbar component codes

--- Invoice-UI/src/app/navbar/navbar.component.ts
+++ Invoice-UI/src/app/navbar/navbar.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
+  ngOnInit(): void {
+    const params = new URLSearchParams(window.location.search);
+    const fromUnifiedDashboard = params.get('fromUnifiedDashboard');
+
+    // Case 1: Redirect if query param is present
+    if (fromUnifiedDashboard === 'yes') {
+      localStorage.setItem('fromUnifiedDashboard', 'true');
+      window.location.href = 'http://localhost:3000/';
+      return;
+    }
+  }
 }
