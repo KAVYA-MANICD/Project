c4b291c68fe915ed469cbbe103b73ad5adc59657
Jupiterking <jupiterkingtechnology@gmail.com>
2025-08-29 14:07:12 +0530

    CORS fix

--- Invoice-API/src/main/java/com/example/Invoice/API/Controller/AiChatController.java
+++ Invoice-API/src/main/java/com/example/Invoice/API/Controller/AiChatController.java
@@ -8,10 +8,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
@@ -20,6 +17,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/chat")
+@CrossOrigin
 public class AiChatController {
 
     private final RestTemplate restTemplate;
